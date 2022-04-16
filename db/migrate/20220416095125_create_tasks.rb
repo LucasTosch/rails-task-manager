9ebedf1171043af6f4e20b3e32b7245ac2a877dc
Lucas Toschini <lucas.toschini@live.fr>
2022-04-16 15:27:41 +0200

    task manager terminé

--- db/migrate/20220416095125_create_tasks.rb
+++ db/migrate/20220416095125_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :title
+      t.text :details
       t.boolean :completed, default: false
 
       t.timestamps
